3e01ec919dac7ff1796cbf5ceab4e5541f429d5f
Justin Dray <justin@dray.be>
2021-01-08 18:18:57 +1000

    Formatting changes

--- expense_tracker/parser.py
+++ expense_tracker/parser.py
@@ -6,15 +6,22 @@ def autodetect(row):
     category = "Unknown"
 
     if match(
-        "GOOD MORNING ASIAN|NIKOS FRUIT|HANARO|DAN MURPHYS|BWS|woolworths|\\bIGA\\b|COLES|ALDI\\b|FOODWORKS|FRESH SENSATIONS|SUMBAL PTY LTD|FRUITS OF EDEN",
-            description):
+        "GOOD MORNING ASIAN|NIKOS FRUIT|HANARO|DAN MURPHYS|BWS"
+        + "|woolworths|\\bIGA\\b|COLES|ALDI\\b|FOODWORKS|FRESH SENSATIONS"
+        + "|SUMBAL PTY LTD|FRUITS OF EDEN|HARRIS FARM MARKETS",
+        description,
+    ):
         # SUMBAL is Brumby's in Nundah
         category = "Groceries"
     elif match("UBER|UNIQLO|MIMCO|ITUNES.COM|HUMBLEBUNDL|STEAM GAMES", description):
         category = cardholder(row)
     elif match("TRANSLINK|NUNDAH STATION", description):
         category = "Public Transport"
-    elif match("PLUME HOLISTIC SKIN|HAIRZOOM|HMB BARBER|TWO BROTHERS TOOMBUL|PURELY CURLS|BLACKWOOD BARBERS", description):
+    elif match(
+        "PLUME HOLISTIC SKIN|HAIRZOOM|HMB BARBER|TWO BROTHERS TOOMBUL"
+        + "|PURELY CURLS|BLACKWOOD BARBERS",
+        description,
+    ):
         category = "Hair"
     elif match("CALTEX|^BP\\b|^PUMA\\b|7-ELEVEN", description):
         category = "Fuel"
@@ -24,11 +31,21 @@ def autodetect(row):
         category = "Work Expense"
     elif match("VETERINARY|PETBARN|Vet", description):
         category = "Pet Expenses"
-    elif match("Excella|MARC MILLER|FRIENDLY CARE|GRK PARTNERS|MEDICARE|MCARE BENEFITS|GRAND UNITED CORPORATE|POST OFFICE SQUARE PHAR|GU HEALTH", description):
+    elif match(
+        "Excella|MARC MILLER|FRIENDLY CARE|GRK PARTNERS|MEDICARE|"
+        + "MCARE BENEFITS|GRAND UNITED CORPORATE|POST OFFICE SQUARE PHAR"
+        + "|GU HEALTH",
+        description,
+    ):
         category = "Health/Medical"
     elif match("Goodlife", description):
         category = "Fitness"
-    elif match("ALDIMOBILE|AMAGICOM|OPTUS|FAMOUS INS|000614696 CLEANING|CRUNCHYROLL|TPG Internet|NETFLIX.COM|AMAZON WEB SERVICES|SPOTIFY|BACKBLAZE|AMZNPRIMEAU MEMBERSHIP", description):
+    elif match(
+        "ALDIMOBILE|AMAGICOM|OPTUS|FAMOUS INS|000614696 CLEANING"
+        + "|CRUNCHYROLL|TPG Internet|NETFLIX.COM|AMAZON WEB SERVICES"
+        + "|SPOTIFY|BACKBLAZE|AMZNPRIMEAU MEMBERSHIP",
+        description,
+    ):
         category = "Untracked"
     elif match("LINKT BRISBANE", description):
         category = "Toll Roads"
